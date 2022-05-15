casbin/node-casbin

fix: *matcher result should be boolean or number* for KeyGet2 (#347)
* fix: *matcher result should be boolean or number* for  KeyGet2

* fix: updated error hint

* fix: basic keyGet2 test

* fix: basic_keyget2_policy.csv

test/enforcer.test.ts
@@ -659,3 +659,10 @@ test('TestBatchEnforce', async () => {
   ];
   expect(await e.batchEnforce(requests)).toEqual([true, true]);
 });
+
+test('TestKeyGet2', async () => {
+  const e = await newEnforcer('examples/basic_keyget2_model.conf', 'examples/basic_keyget2_policy.csv');
+  expect(await e.enforce('alice', 'data')).toBe(false);
+  expect(await e.enforce('alice', '/data')).toBe(false);
+  expect(await e.enforce('alice', '/data/1')).toBe(true);
+});

casbin/casbin-core

(cherry picked from commit 0257078e1302f5ef081ec143afe5c470f4f862e9)
Signed-off-by: Zixuan Liu <nodeces@gmail.com>

test/enforcer.test.ts
@@ -726,3 +726,10 @@ test('new EnforceContextEX config', async () => {
   await expect(e.enforceEx(enforceContext, 'alice', 'data1', 'read')).resolves.toStrictEqual([true, ['alice', 'data1', 'read']]);
   await expect(e.enforceEx(enforceContext, 'bob', 'data2', 'write')).resolves.toStrictEqual([true, ['bob', 'data2', 'write']]);
 });
+
+test('TestKeyGet2', async () => {
+  const e = await getEnforcerWithPath('examples/basic_keyget2_model.conf', 'examples/basic_keyget2_policy.csv');
+  expect(await e.enforce('alice', 'data')).toBe(false);
+  expect(await e.enforce('alice', '/data')).toBe(false);
+  expect(await e.enforce('alice', '/data/1')).toBe(true);
+});